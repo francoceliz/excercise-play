b514cca2037472537f2c7997435812e44232ed07
CARLOS-V-G <cv5944341@gmail.com>
2022-11-08 20:53:08 -0300

    modifico condicionales.js

--- condicionales.js
+++ condicionales.js
@@ -7,7 +7,7 @@ if (sabado== "hoy no es feriado"){
     console.log("es false");
 }
 
-let marte= "hoy se trabaja"
-if (marte== "hoy se trabaja"){
+let lunes= "hoy se trabaja"
+if (lunes== "hoy se trabaja"){
     console.log("es true");
 }
