363c3fb78d5db31e86afe77ab2ff13891d34655d
CARLOS-V-G <cv5944341@gmail.com>
2022-11-08 19:40:23 -0300

    agrego mas condicionales

--- condicionales.js
+++ condicionales.js
@@ -6,3 +6,8 @@ let sabado= "hoy no es feriado"
 if (sabado== "hoy no es feriado"){
     console.log("es false");
 }
+
+let lunes= "hoy se trabaja"
+if (lunes == "hoy se trabaja"){
+    console.log("es true");
+}
