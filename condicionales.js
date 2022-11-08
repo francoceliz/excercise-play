1acdecedd2f5e81229910c95ccaf92caffc2fd68
francoceliz <franco.celiz.1999@gmail.com>
2022-11-08 20:28:44 -0300

    Empezamos de nuevo

---
+++ condicionales.js
@@ -0,0 +1,8 @@
+let dato= 'hoy es feriado'
+if (dato== 'hoy es feriado'){
+    console.log("es true");
+}
+let sabado= "hoy no es feriado"
+if (sabado== "hoy no es feriado"){
+    console.log("es false");
+}
