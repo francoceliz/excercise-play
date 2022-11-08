d3a00845575cd226ab192c84e22ada8a61cd5af1
francoceliz <franco.celiz.1999@gmail.com>
2022-11-08 19:19:02 -0300

    archivos borrados

--- app.js
+++
@@ -1,8 +0,0 @@
-const fs = rerequire('fs');
-const dataPersona = require('./objetoLiteral')
-console.log(dataPersona.nombre);
-const dataPersonaJSON = fs.readFileSync('./personaJSON.json', 'utf-8')
-let nombreCompleto = dataPersona + " " + dataPersona.apellido
-
-console.log(typeof dataPersona)
-console.log(typeof dataPersonaJSON)
